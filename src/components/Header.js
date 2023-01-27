8723dc08f7cb3308ca8ab86c3e13240923b33ae6
emreharman <emrehrmnb@gmail.com>
2023-01-28 00:31:37 +0300

    week2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,33 @@
 import React from "react";
 
-import '../assets/styles/header.css'
-import logo from "../assets/imgs/logo.gif"
+import "../assets/styles/header.css";
+import logo from "../assets/imgs/logo.gif";
+import leftArrow from "../assets/imgs/leftArrow.png";
 
-const Header=()=>{
-    return(
-        <div className="headerContainer">
-            <div className="headerWrapper">
-                <div className="logoWrapper">
-                    <img src={logo} />
-                </div>
-                <h1>My Budget App</h1>
-            </div>
+import { useNavigate } from "react-router-dom";
+
+const Header = ({ whichPage, navigateTo }) => {
+  console.log(whichPage);
+  const navigate=useNavigate()
+  var show = false;
+  if (whichPage !== "home") {
+    show = true;
+  }
+  return (
+    <div className="headerContainer">
+      <div className="headerWrapper">
+        {show === true && (
+          <div onClick={()=>navigate(navigateTo)} className="headerIconWrapper">
+            <img src={leftArrow} />
+          </div>
+        )}
+        <div className="logoWrapper">
+          <img src={logo} />
         </div>
-    )
-}
+        <h1>My Budget App</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
